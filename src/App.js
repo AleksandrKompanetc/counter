322cc62c4295e6b2f385f8c2392356b716e944f7
AleksandrKompanetc <moneymacker7@gmail.com>
2024-12-24 23:56:04 +0200

    creating functions onClickPlus and onClickMinus

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const onClickPlus = () => {
+    setCount(count + 1)
+  }
+
+  const onClickMinus = () => {
+    setCount(count - 1)
+  }
+
   return (
     <div className="App">
       <div className='container'>
         <h2 className='counter'>Counter:</h2>
-        <h1 className='count'>0</h1>
-        <button className='plus'>+</button>
-        <button className='minus'>-</button>
+        <h1 className='count'>{count}</h1>
+        <button onClick={onClickPlus} className='plus'>+</button>
+        <button onClick={onClickMinus} className='minus'>-</button>
       </div>
     </div>
   );
