be02ef1af9da67b228574fb5c721bb31c0922870
AleksandrKompanetc <moneymacker7@gmail.com>
2024-12-24 22:38:27 +0200

    adding app counter

--- src/App.js
+++ src/App.js
@@ -3,7 +3,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <div className='container'>
+        <h2 className='counter'>Counter:</h2>
+        <h1 className='count'>0</h1>
+        <button className='plus'>+</button>
+        <button className='minus'>-</button>
+      </div>
     </div>
   );
 }
